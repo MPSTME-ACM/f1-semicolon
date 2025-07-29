c9d3328e4aa459faf953ce4cb66216abc545a0b1
Parth Gupta <parthgupta211206@gmail.com>
2025-07-29 15:11:11 +0530

    add port configuration variables

--- server.ts
+++ server.ts
@@ -1,12 +1,16 @@
+import * as dotenv from 'dotenv';
 import { createServer } from 'http';
 import { parse } from 'url';
 import next from 'next';
 import { Server } from 'socket.io';
 import type { Player, LobbyData } from './src/app/types';
 
-const dev = false;
-const hostname = '0.0.0.0'; 
-const port = 3000;
+// Load environment variables from the .env file
+dotenv.config();
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = process.env.HOSTNAME || '0.0.0.0'; 
+const port = parseInt(process.env.PORT || '3000', 10);
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
